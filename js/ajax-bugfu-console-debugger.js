99cdbceef0329c9507fb1ebcef28ea3c826aa818
Federico Andrioli <fedewave@gmail.com>
2016-11-04 17:12:11 +0100

    fix webserver overload issue

--- js/ajax-bugfu-console-debugger.js
+++ js/ajax-bugfu-console-debugger.js
@@ -1,38 +1,61 @@
 (function($) {
 	
-	var bugfu_previous_log;
-	var bugfu_read_log_interval;
+	var bugfu_can_read_log = true;
+	var bugfu_previous_log = null;
+	var bugfu_read_log_interval = null;
+	var bugfu_interval_millisec = 350;
 
 	$( document ).ready( function() {
 
-		bugfu_read_log_interval = setInterval( bugfu_read_log, 300 );
+		bugfu_read_log_interval = setInterval( bugfu_read_log, bugfu_interval_millisec );
+	});
+
+	$( document ).ajaxSend( function() {
+
+		if( bugfu_read_log_interval == null ) {
+
+			bugfu_read_log_interval = setInterval( bugfu_read_log, bugfu_interval_millisec );
+		}
 	});
 
 	function bugfu_read_log() {
 
-		var data = {
-			action: 'bugfu_ajax_read_debug_log'
-		}
+		if( bugfu_can_read_log ) {
 
-		$.ajax({
-			url: bugfu_console_debugger_ajax_params.ajaxurl,
-			type: 'get',
-			data: data,
-			success: function( response ) {
+			bugfu_can_read_log = false;
 
-				var preheader = "\n";
-				var header = response.data.header;
-				var log = response.data.log;
-
-				if( (log && log != bugfu_previous_log) || bugfu_previous_log == null ) {
-					
-					console.log(preheader.concat(header, "\n\n", log, "\n\n\n"));
-
-					bugfu_previous_log = log;
-				}
-
+			var data = {
+				action: 'bugfu_ajax_read_debug_log'
 			}
-		})
+
+			$.ajax({
+				url: bugfu_console_debugger_ajax_params.ajaxurl,
+				type: 'get',
+				data: data,
+				success: function( response ) {
+
+					var preheader = "\n";
+					var header = response.data.header;
+					var log = response.data.log;
+
+					if( log || bugfu_previous_log == null ) {
+						
+						console.log(preheader.concat(header, "\n\n", log, "\n\n\n"));
+						bugfu_previous_log = log;
+					}
+
+					clearInterval(bugfu_read_log_interval);
+					bugfu_read_log_interval = null;
+					bugfu_can_read_log = true;
+				},
+				error: function(){
+					
+					clearInterval(bugfu_read_log_interval);
+					bugfu_read_log_interval = null;
+					bugfu_can_read_log = true;
+				} 
+			});
+		}
 	}
 
 })( jQuery );
